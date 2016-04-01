342df377dbd57b02388bab44b0ad7e2662adbb9b
Mikael Borgbrant <mikael.borgbrant@semcon.com>
2016-04-01 13:29:11 +0200

    Update 2016-04-01T11:29:05.569Z

--- src/js/rom.js
+++
@@ -1,8 +0,0 @@
-const Rom = function (baseAddress, memoryDump) {
-	this.baseAddress = baseAddress;
-	this.memoryDump = memoryDump;
-}
-
-Rom.prototype.onReadByte = function (address) {
-	return this.memoryDump[address - this.baseAddress]
-}
